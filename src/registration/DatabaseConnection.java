a15153cd9f2353f239eb1cfdc2d6527f02a11ecd
ShielaMaeBacalando <chooksterchookie@gmail.com>
2026-04-09 23:18:46 +0800

    LABAD SA SIYA

--- src/registration/DatabaseConnection.java
+++ src/registration/DatabaseConnection.java
@@ -1,40 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package registration;
+package registration; 
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;       
-import java.util.logging.Level;
-import java.util.logging.Logger;
-/**
- *
- * @author Chookie
- */
-public class DatabaseConnection { 
+import java.sql.SQLException;
+
+public class DatabaseConnection {
     
-    //Database Credentials
     private static final String URL = "jdbc:mysql://localhost:3306/useregistration";
     private static final String USER = "root";
-    private static final String PASSWORD = "";     
-    
-    public static Connection getConnection(){
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() {
         Connection conn = null;
-        
-        try (
-            conn = DriverManager.getConnection(url:URL, user:USER, password:PASSWORD);
-            System.out.println(x: "Database Connected Succesfully!");
-          
-        } catch (SQLException e) {
-           System.out.println(x: "Connection Failed!");
-           e.printStackTrace();
+        try {
+            
+            Class.forName("com.mysql.cj.jdbc.Driver"); 
+            
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Database Connected Successfully!");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Connection Failed!");
+            e.printStackTrace();
         }
-        
         return conn;
-                
-                
     }
-    
 }
